f372c63f386c3d0bc9a1bbc3d4334551c289e6e9
Clayton <clayton3660@gmail.com>
2022-02-26 11:56:50 -0300

    propriedade_do_componente

--- pages/basicos/usandoTitulo.jsx
+++ pages/basicos/usandoTitulo.jsx
@@ -3,7 +3,16 @@ import Titulo from '../../components/Titulo'
 export default function usandoTitulo() {
   return (
     <div>
-      <Titulo />
+      <Titulo 
+        principal = "Página de Cadastro"
+        secundario = "Incluir, alterar e excluir coisas!" 
+      />
+      <Titulo 
+        principal = "Página de Login"
+        secundario = "Informe seu email e senha" 
+        pequeno
+        // pode ser apenas o nome da condição, ele entende que se tem é verdadeiro, assim não preciasando colocar = {true}
+      />
     </div>
   )
 }
