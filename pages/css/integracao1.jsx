4c32dd58ce6aa4b1027b627f3f37683cc215f65c
Clayton <clayton3660@gmail.com>
2022-02-28 00:19:21 -0300

    ajuste_integracao_com_css_#01

--- pages/css/integracao1.jsx
+++ pages/css/integracao1.jsx
@@ -1,6 +1,8 @@
+// Não posso inportar aqui!!!
+// O import foi feito dentro de _app.js
 export default function integracao() {
   return (
-    <div>
+    <div className="integracao1">
       <div className="vermelha">Texto #1</div>
       <div className="azul">Texto #2</div>
       <div className="branca">Texto #3</div>
