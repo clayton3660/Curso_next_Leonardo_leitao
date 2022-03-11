d9b3fadc3dd3814f20d198694e7780f46a739f7f
Clayton <clayton3660@gmail.com>
2022-03-11 10:25:12 -0300

    renderizacao_condicional_#02

---
+++ pages/render/condicional2.jsx
@@ -0,0 +1,15 @@
+import If from "../../components/if";
+
+export default function condicional2 () {
+  const numero = 2
+  return (
+    <div>
+      <If teste={numero % 2 === 0}>
+        <h1>O número {numero} é par</h1>
+      </If>
+      <If teste={numero % 2 === 1}>
+        <h1>O número {numero} é ímpar</h1>
+      </If>
+    </div>
+  )
+}
