000ed817e1855622c854ac6c5501e88fcd714719
Clayton <clayton3660@gmail.com>
2022-05-26 11:33:32 -0300

    adicionando_eventos_a_porta_#02

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const [ p1, setP1 ] = useState(new PortaModel(1))
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <Porta value={p1} />
+      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)} />
     </div>
   )
 }
