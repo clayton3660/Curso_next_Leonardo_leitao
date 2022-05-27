0c82147ba2a04a35df61d4beef90b79e00f23403
Clayton <clayton3660@gmail.com>
2022-05-27 11:07:01 -0300

    lidando_com_array_de_portas_#02

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,21 @@
 import { useState } from "react";
 import Porta from "../components/Porta";
-import { criarPortas } from "../functions/portas";
+import { atualizarPortas, criarPortas } from "../functions/portas";
 import PortaModel from "../model/porta";
 
 export default function Home() {
-  const [ p1, setP1 ] = useState(new PortaModel(1))
-  
-  console.log(criarPortas(3, 2))
+  const [portas, setPortas] = useState(criarPortas(3, 2))
+
+  function rendenrizarPortas() {
+    return portas.map(porta => {
+      return <Porta key={porta.numero} value={porta} 
+      onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))} />
+    })
+  }
   
   return (
     <div style={{ display: 'flex'}}>
-      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)} />
+      {rendenrizarPortas()}
     </div>
   )
 }
