6ae8bc74cc9d261516d8b8bcf9b97513ed7fc268
Clayton <clayton3660@gmail.com>
2022-06-17 22:57:46 -0300

    componente_jogo_#01

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,10 @@ import { atualizarPortas, criarPortas } from "../functions/portas";
 import PortaModel from "../model/porta";
 
 export default function Home() {
-  const [portas, setPortas] = useState(criarPortas(3, 2))
-
-  function rendenrizarPortas() {
-    return portas.map(porta => {
-      return <Porta key={porta.numero} value={porta} 
-      onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))} />
-    })
-  }
   
   return (
     <div style={{ display: 'flex'}}>
-      {rendenrizarPortas()}
+      <h1>Fomulário de início !!!</h1>
     </div>
   )
 }
