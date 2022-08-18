9e7a41e2d170266c2ad626c054e89c143e1ecbc8
Clayton <clayton3660@gmail.com>
2022-08-17 22:08:36 -0300

    finalizando_o_formulario

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,36 @@
 import styles from "../styles/Formulario.module.css"
 import Cartao from "../components/Cartao";
 import Link from "next/link";
+import EntradaNumerica from "../components/EntradaNumerica";
+import { useState } from "react";
 
 
 
 export default function Formulario() {
+
+  const [qtdePortas, setQtdePortas] = useState(0)
+  const [comPresente, setComPresente] = useState(0)
+
   return (
     <div className={styles.formulario}>
       <div>
         <Cartao bgcolor="#c0392c">
           <h1>Monty Hall</h1>
         </Cartao>
-        <Cartao></Cartao>
+        <Cartao>
+          <EntradaNumerica text="Qtde Portas?" 
+           value={qtdePortas} onChange={novaQtde => setQtdePortas(novaQtde)} 
+          />
+        </Cartao>
       </div>
       <div>
-        <Cartao></Cartao>
+        <Cartao>
+        <EntradaNumerica text="Porta com Presente?" 
+          value={comPresente} onChange={novaPortaComPresente => setComPresente(novaPortaComPresente)} 
+        />
+        </Cartao>
         <Cartao bgcolor="#28a085">
-          <Link href={`/jogo/4/2`}>
+          <Link href={`/jogo/${qtdePortas}/${comPresente}`}>
             <h2 className={styles.link}>Iniciar</h2>
           </Link>
         </Cartao>
